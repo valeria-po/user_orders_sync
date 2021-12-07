6a7829ed06396c4b9f500805678bdd54a7388e7a
Valerie <valerie.postolachi@gmail.com>
2021-12-07 18:15:36 +0200

    Adds scheduler functionality

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from order_helpers.sync_orders import sync_db
 
 app = Flask(__name__)
 
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(sync_db, 'interval', minutes=1)
+scheduler.start()
+
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
--- order_helpers/sync_orders.py
+++ order_helpers/sync_orders.py
@@ -1,6 +1,3 @@
-# 1. pymongo - get, (select) from mongodb
-
-# 2. if postgres == 0, load everything, else: load diff - set
 import pandas as pd
 from prod_db import get_pd_users_orders
 from wh_db import get_wh_users_orders, write_to_db
@@ -17,10 +14,6 @@ def sync_db() -> str:
     if wh_items.equals(prod_items):
         return "DBs are synced"
     else:
-        # new_items = set(prod_items) ^ set(wh_items) find difference by using set on ids
         new_items = pd.concat([prod_items, wh_items]).drop_duplicates(keep=False)
         write_to_db(new_items)
         return f"Added {len(new_items)} new items"
-
-
-sync_db()
